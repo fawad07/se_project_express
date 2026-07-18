fe5f7b8dbf0fe5e5015d6874d536147362018de3
Fawad <muhammad.f.aleem@gmail.com>
2026-07-18 16:17:23 -0500

    fix/updated via feedback

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,53 +1,42 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-// Define the ClothingItem schema
 const clothingItemSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 2,
-      maxlength: 30,
-    },
-    weather: {
-      type: String,
-      required: true,
-      enum: ['hot', 'warm', 'cold'],  // Only allows 'hot', 'warm', or 'cold'
-    },
-    imageUrl: {
-      type: String,
-      required: true,
-      validate: {
-        validator(value) {
-          return validator.isURL(value);
-        },
-        message: 'You must enter a valid URL',
-      } 
-      /*{
-        validator: function (v) {
-          return /^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(v);  // Basic URL validation for images
-        },
-        message: (props) => `${props.value} is not a valid URL!`,
-      }*/,
-    },
-    owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'user',
-      required: true,
-    },
-    likes: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
       },
-    ],
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      required: true,
+      message: "You must enter a valid URL",
     },
-  });
-
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("clothingItem", clothingItemSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const userRoute = require("./users");
 const clothingItemRoute = require("./clothingItem");
+const { NOT_FOUND_ERROR_CODE } = require("../utils/errors");
 
-router.use('/users', userRoute);
+router.use("/users", userRoute);
 router.use("/items", clothingItemRoute);
 
 router.use((req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND_ERROR_CODE).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
